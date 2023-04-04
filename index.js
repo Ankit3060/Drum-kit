61e557d39f86f82d1f95ab669cef647a187c3cf7
Ankit3060 <121934648+Ankit3060@users.noreply.github.com>
2023-04-04 23:47:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,37 +31,37 @@ function makeSound(key)
     switch(key)
         {
             case "w":
-                var crash=new Audio('sounds/crash.mp3');
+                var crash=new Audio('crash.mp3');
                 crash.play();
                 break;
             
             case "a":
-                var kick=new Audio('sounds/kick-bass.mp3');
+                var kick=new Audio('kick-bass.mp3');
                 kick.play();
                 break;
 
             case "s":
-                var snare=new Audio('sounds/snare.mp3');
+                var snare=new Audio('snare.mp3');
                 snare.play();
                 break;
 
             case "d":
-                var tom1=new Audio('sounds/tom-1.mp3');
+                var tom1=new Audio('tom-1.mp3');
                 tom1.play();
                 break;
 
             case "j":
-                var tom2=new Audio('sounds/tom-2.mp3');
+                var tom2=new Audio('tom-2.mp3');
                 tom2.play();
                 break;
 
             case "k":
-                var tom3=new Audio('sounds/tom-3.mp3');
+                var tom3=new Audio('tom-3.mp3');
                 tom3.play();
                 break;
 
             case "l":
-                var tom4=new Audio('sounds/tom-4.mp3');
+                var tom4=new Audio('tom-4.mp3');
                 tom4.play();
                 break;
 
